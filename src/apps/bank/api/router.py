0340535cdc1de66be8003a8bab1bfeceabc66fcc
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-02-11 03:08:08 +0700

    create serializer for transactions and category

--- src/apps/bank/api/router.py
+++ src/apps/bank/api/router.py
@@ -2,10 +2,13 @@ from django.urls import path
 from rest_framework import routers
 
 
-from apps.bank.api.views import CurrencyListAPIView
+from apps.bank.api.views import CurrencyListAPIView, TransactionListAPIView, CategoryListAPIView
 
 routes = routers.SimpleRouter()
 
 urlpatterns: list = [
-    path("curencies", CurrencyListAPIView.as_view(), name="currencies")
+    path("curencies", CurrencyListAPIView.as_view(), name="currencies"),
+    path("categories", CategoryListAPIView.as_view(), name="categories"),
+    path("transactions", TransactionListAPIView.as_view(), name="transactions"),
+
 ]
--- src/apps/bank/api/views.py
+++ src/apps/bank/api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.generics import ListAPIView
 
-from apps.bank.models import CurrencyModel
-from apps.bank.api.serializers import CurrencySerializer
+from apps.bank.models import CurrencyModel, CategoryModel, TransactionModel
+from apps.bank.api.serializers import CurrencySerializer, CategorySerializer, TransactionSerializer
 
 class CurrencyListAPIView(ListAPIView):
     queryset = CurrencyModel.objects.all()
     serializer_class = CurrencySerializer
+    
+class CategoryListAPIView(ListAPIView):
+    queryset = CategoryModel.objects.all()
+    serializer_class = CategorySerializer
+
+class TransactionListAPIView(ListAPIView):
+    queryset = TransactionModel.objects.all()
+    serializer_class = TransactionSerializer
