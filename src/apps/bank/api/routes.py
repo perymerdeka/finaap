e4c3ec895e4cf578e4993e935878726672b16a0a
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-02-13 06:45:57 +0700

    adding currency and category viewset

--- src/apps/bank/api/routes.py
+++ src/apps/bank/api/routes.py
@@ -2,16 +2,16 @@ from django.urls import path
 from rest_framework import routers
 
 
-from apps.bank.api.views import CurrencyListAPIView, CategoryViewSet, TransactionModelViewSet
+from apps.bank.api.views import CurrencyModelViewSet, TransactionModelViewSet, CategoryModelViewSet
 
 router = routers.SimpleRouter()
 
 
 # register viewset
-router.register(r"categories", CategoryViewSet, basename="category") # category-list
+router.register(r"categories", CategoryModelViewSet, basename="category") # category-list
 router.register(r"transactions", TransactionModelViewSet, basename="transaction")
+router.register(r"currencies", CurrencyModelViewSet, basename="currency")
 
-urlpatterns: list = [
-    path("curencies", CurrencyListAPIView.as_view(), name="currencies"),    
+urlpatterns: list = [   
 
 ] + router.urls # <- kita daftarkan disini
